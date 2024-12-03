40dc0ec7fd9562656006fce5dcd64722811b6107
lucifer7303 <jaiswalansh7103@gmail.com>
2024-12-03 20:50:15 +0530

    added caching to other service methods

--- src/main/java/com/MangaLib/MangaLib/Manga/MangaService.java
+++ src/main/java/com/MangaLib/MangaLib/Manga/MangaService.java
@@ -22,23 +22,25 @@ public class MangaService {
         this.mangaDexProxy = mangaDexProxy;
 
     }
-    @Cacheable(value = "searchResultDTO" , key = "#title + #limit + #offset")
     @RateLimiter(name="def",fallbackMethod = "searchFallback")
+    @Cacheable(value = "searchResultDTO" , key = "#title + #limit + #offset")
     public Optional<SearchResultDTO> search(@RequestParam String title, @RequestParam Integer limit, @RequestParam Integer offset){
-        System.out.println("R-U-N-N-I-N-G  S-E-A-R-C-H  S-E-R-V-I-C-E  M-E-T-H-O-D");
         return Optional.of(mangaDexProxy.search(title,limit,offset));
     }
     @RateLimiter(name="def",fallbackMethod = "mangaFallback")
+    @Cacheable(value = "mangaDTO" , key = "#id")
     public Optional<MangaDTO> getDetails(@PathVariable String id){
         return Optional.of(mangaDexProxy.getDetails(id));
     }
 
     @RateLimiter(name="def",fallbackMethod = "chapterFallback")
+    @Cacheable(value = "chaptersListDTO" , key = "#id + #limit + #offset")
     public Optional<ChaptersListDTO> getChaptersList(String id,int limit,int offset){
         return Optional.of(mangaDexProxy.getChaptersList(id,"en","asc",limit,offset));
     }
 
     @RateLimiter(name="atHome",fallbackMethod = "pageFallback")
+    @Cacheable(value = "chapterDTO" , key = "#id")
     public Optional<ChapterDTO> getPages(String id){
         return Optional.of(mangaDexProxy.getPages(id));
     }
@@ -46,9 +48,7 @@ public class MangaService {
     private Optional<MangaDTO> mangaFallback(String id,Throwable e){
         return Optional.empty();
     }
-    private Optional<SearchResultDTO> searchFallback(String title,Integer limit,Integer offset,Throwable e){
-        System.out.println("Fallback encountered");
-        return Optional.empty();
+    private Optional<SearchResultDTO> searchFallback(String title,Integer limit,Integer offset,Throwable e){return Optional.empty();
     }
     private Optional<ChaptersListDTO> chapterFallback(String id,int limit,int offset,Throwable e){
         return Optional.empty();
